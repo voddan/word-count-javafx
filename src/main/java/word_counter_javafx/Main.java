2c88657e7726560db9c4f3e38c18cfa4dbd1b33c
voddan <dgvodopyan@gmail.com>
2016-04-03 23:05:16 +0300

    fixed access to resources; launches now

--- src/main/java/word_counter_javafx/Main.java
+++ src/main/java/word_counter_javafx/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
         Parent root = loader.load();
         controller = loader.getController();
         controller.stage = primaryStage;
